409c92d51a89395a4c431c3201ad2b21c736d2e4
Your Name <you@example.com>
2024-10-13 14:42:04 +0600

    added another code

---
+++ even_odd.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+int i,n;
+cout <<"input a number: ";
+cin >> i;
+
+if(i!=0){
+
+if(i%2==0)
+{
+
+    cout << "the number is even";
+}
+else {
+
+
+    cout << "the number is odd";
+}
+}
+else{
+    cout << "the numer is even ";
+}
+
+
+
+    return 0;
+}
