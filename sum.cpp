5519a176685908a23b94eac8680c72f64c789171
Your Name <you@example.com>
2024-10-13 14:25:15 +0600

    first commit

---
+++ sum.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x,o,u; 
+cout << "Type 1st number: "; // Type a number and press enter
+cin >> x;
+cout << "Type 2nd number: "; // Type a number and press enter
+cin >> o;
+cout << "Type 3rd number: "; // Type a number and press enter
+cin >> u;
+int n=x+o+u;
+ // Get user input from the keyboard
+cout << "Your number is: " << n; // Display the input value
+    return 0;
+}
